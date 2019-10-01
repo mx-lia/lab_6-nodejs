c88200c76b249ec77da6102463a06aade0bfdbeb
Yuliya Maximchikova <m.yuliyas.00@gmail.com>
2019-10-01 10:42:14 +0300

    doesn't work response

--- 06-02.js
+++ 06-02.js
@@ -1,13 +1,20 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-const sendmail = require('sendmail');
+const {parse} = require('querystring');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-    if(url.parse(request.url).pathname == '/') {
-        fs.readFile('./index.html', (data, err) => {
+    if(url.parse(request.url).pathname === '/' && request.method == 'GET') {
+        fs.readFile('./index.html', (err, data) => {
             response.end(data);
+        });
+    } else if (url.parse(request.url).pathname === '/' && request.method == 'POST') {
+        let body = '';
+        request.on('data', (data) => body += data);
+        request.on('end', () => {
+            let parm = parse(body);
+            response.end('From: ' + parm.from + ' To: ' + parm.to + ' Text: ' + parm.text);
         });
     }
 }).listen(3000);
